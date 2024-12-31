70280698c717796a71be69eb19797aa63fd204a8
Noir <89527725+noir157@users.noreply.github.com>
2024-12-31 20:43:54 -0300

    Add files via upload

---
+++ ngt/members.js
@@ -0,0 +1,26 @@
+function toggleMembersList() {
+    const membersList = document.getElementById('members-list');
+    const membersIcon = document.getElementById('members-icon');
+    const isExpanded = membersList.classList.contains('expand');
+
+    if (isExpanded) {
+        membersList.classList.remove('expand');
+        membersIcon.classList.remove('rotate');
+    } else {
+        membersList.classList.add('expand');
+        membersIcon.classList.add('rotate');
+    }
+}
+
+function redirectToMember(memberName) {
+    let url;
+    if (memberName === 'Noir') {
+        url = 'https://noirz.site/';
+    } else if (memberName === 'Emmanuel') {
+        url = 'https://noirz.site/ngt';
+    }
+
+    if (url) {
+        window.location.href = url;
+    }
+}
